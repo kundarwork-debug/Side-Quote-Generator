3386740e659f5104e4571eae83abd65dbee4f9c3
Prasad Kundar <kundar.work@gmail.com>
2026-08-23 22:19:12 +0530

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'apml-portal-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './404.html',
+  './common.css?v=2.2',
+  './common.js?v=2.2',
+  './manifest.json',
+  './logo.svg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
